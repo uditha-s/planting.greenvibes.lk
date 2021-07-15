2681502321c99424cee4b1f292681b0e890c8097
naveensachintha <naveensachintha.11@gmail.com>
2021-07-15 19:38:30 +0530

    ABOUT

--- about-us.php
+++ about-us.php
@@ -6,29 +6,40 @@
 <?php require_once('inc/header.php'); ?>
   <div class="text-box">
         <h1>Who We Are ?</h1>
-        <p>ShareTheMeal is a crowdfunding smartphone application to fight global hunger through the United Nations World Food Programme (WFP).</p>
+        <p>Today, more and more companies are creating products that help us live a green lifestyle. </p>
         <!-- <a href="" class="visit-btn">Visit us</a> -->
         </div>
     </section>
 <body>
 
 <section class="content">
-<h2>About Us</h2>
-<p>We are Greenvibes Technologies. A team with a diversified and forward thinking capabilities in the field of software with the elements of creating unique solutions.
+<h2>About Greenvibes planting</h2>
+<p>What is greenvibes planting and what about it? Greenvibes planting relates to how to conserve environment and improved health of the environment. 
+Here is an article on how to go green, and discussing the importance of eco-friendly products, 
+including supporting practices like smart applications of green energy products and the strategies for green energy maintenance, as well as the advantages.
 
-Just as machines have extended man's mechanical power and his convenience and comfort, Information Technology as commonly pictured by computers, 
-is extending man's mind or brain or intellectual power. 
-This paved the way for us to think out of the box and come up with systems spending our time and effort targeting areas that are untouched.
+Today, more and more companies are creating products that help us live a green lifestyle. 
+Everything from cars, light bulbs, utensils, straws, cleaners, mattresses, and clothing is created with environmental preservation in mind.
 
-advancing knowledge through research, and disseminating knowledge by means of the internet, researching and writing books, producing newsletters, editorials, monographs, seminars, lectures, conferences and the like.</p>
+Greenvibes Planting” relates to the concerns for environmental conservation and improved health of the environment. 
+This includes supporting practices like informed consumption, conservation practices and investment in renewable energy
+
+</p>
 
 
-<h2>Our Vision</h2>
-<p>Our Vision is to empower entrepreneurs with a roadmap and support that guides them through the transition from start-ups, to micro-enterprises, 
-and on to small and medium-sized businesses that create jobs, lead innovation, and increase economic development in their local communities.</p>
+<h2>Our Master Plan</h2>
+<p>Master Plan is a living document that communicates a sustainable vision for the next 100 years. By setting the direction for current and future residents, 
+development of the physical manifestations upon the site continues to support this sustainable vision and even builds off of them for generations to come. 
+Each green space has been developed to determine the arrangement of water elements, circulation patterns, structure locations, parking zones, and new landscapes.</p>
 
 <h2>Our Mission</h2>
-<p>Incorporating forward thinking with imagination, creativity, and originality to reach our goal to become a leader in software solutions.
+<p>These arrangements are coupled with principles of design that center on sustainability, which inform the final layout and material choices for each garden space.<br>
+	<br>
+	A flourishing life on land is the foundation for our life on this planet
+<br>
+<br>
+We are all part of the planet’s ecosystem and we have caused severe damage to it through deforestation, 
+loss of natural habitats and land degradation. Promoting a sustainable use of our ecosystems and preserving biodiversity is not a cause. It is the key to our own survival.
 </p>
 </section>
 </body>
--- index.php
+++ index.php
@@ -5,14 +5,16 @@
 <?php $title = 'Home - '; ?>
 <?php require_once('inc/header.php'); ?>
   <div class="text-box">
-        <h1>Make it simple. But significant</h1>
-        <p>ShareTheMeal is a crowdfunding smartphone application to fight global hunger through the United Nations World Food Programme (WFP).</p>
+        <h1>One Plant For Future</h1>
+        <p>We are all part of the planet’s ecosystem and we have caused severe damage to it through deforestation, 
+loss of natural habitats and land degradation. Promoting a sustainable use of our ecosystems and preserving biodiversity is not a cause. It is the key to our own survival.</p>
         <a href="#content" class="visit-btn">Visit us</a>
         </div>
     </section>
 <body>
 <section class="content" id="content">
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae voluptatum tempore, non, perferendis reiciendis ratione, ut tenetur exercitationem voluptatibus quisquam ex sint veritatis mollitia fugit laudantium. Accusantium, illo! Adipisci, aut!</p>
+  <p>Today, more and more companies are creating products that help us live a green lifestyle. 
+Everything from cars, light bulbs, utensils, straws, cleaners, mattresses, and clothing is created with environmental preservation in mind.</p>
 
 
 </section>
