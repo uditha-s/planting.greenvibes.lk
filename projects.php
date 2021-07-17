76aebf28f840babf74e9129f000c69297d1d940e
uditha-s <albauditha@gmail.com>
2021-07-17 11:35:26 +0530

    Update projects.php

--- projects.php
+++ projects.php
@@ -5,38 +5,57 @@
 <?php $title = 'Projects - '; ?>
 <?php require_once('inc/header.php'); ?>
   <div class="text-box">
-        <h1>Our Services</h1>
-        <p>ShareTheMeal is a crowdfunding smartphone application to fight global hunger through the United Nations World Food Programme (WFP).</p>
+        <h1>your'e task</h1>
+        <p>we are live on earth.we must protect it.
+tree planting is one of the best to do this.<br>
+tree planting is a major role of united nation's "life on land" globle goal. <br>greenvibes us hope to encourage you to plant as much as you can. .</p>
         <!-- <a href="" class="visit-btn">Visit us</a> -->
         </div>
     </section>
 <body>
 
-<section class="content"><h2>we wish to do</h2>
-<p>Have you hit a dead end with brainstorming marketing ideas for your technology company? 
-Are your current technology product marketing strategies falling short of your goals? Do you need some help improving your technology marketing plan?<br><br>
+<section class="content"><h2>your'e task</h2><br>
+<h2>Plant can be called as memories.</h2><br>
+<p>because it lasts a long time after planting. So plant a tree for your special occasions.(ex- family member's birthday,religious festivals,january'1st...ect)
+When you see the plant, you will remember your memories And it will bring you many benefits.
+For this it is advisable to use long-term plants.
+It has a positive effect on biodiversity.</p><br>
+<h2>Next comes the fruit season.</h2><br>
+<p>Collect nuts from the fruit you eat during that season. Then when you go to work or on another trip, Throw your seeds into the empty fields on either side of the road.
+It can increase the plant population on the land.
+This is a very simple thing.. <br><br>
 
-Then, keep reading! This article shares the best marketing strategies for IT companies and tech organizations so you can focus your time, energy, 
-and resources on the top technology marketing tactics. <br> <br>
+<h2>PLANTS YOU CAN USE FOR THIS</h2>
+<ul>
+ 
+ 
+<li><a href="https://en.wikipedia.org/wiki/Artocarpus_sericicarpus">Bulu</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Terminalia_arjuna">Kubuk</a></li>
+<li><a href="https://www.dilmahconservation.org/arboretum/plants-and-trees/mee--a105cfb6730a9931864a3bb78c49252b.html">Mee</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Coscinium_fenestratum">Maara</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Jackfruit">Jack fruit</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Mango">Mango</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Phyllanthus_emblica">Nelli</a></li>
+<li><a href="https://www.dilmahconservation.org/arboretum/plants-and-trees/wal-ehala--2f07674d6ee928d1ed17022cf97389a4.html">Wal ahela</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Peltophorum_pterocarpum">Copper Pod Tree</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Mesua_ferrea">Domba</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Amherstia">Kukul Kakul</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Tabebuia_rosea">Roborosia</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Barringtonia_asiatica"> Mudilla </a></li>
+<li><a href="https://en.wikipedia.org/wiki/Millettia_pinnata">Magul Karanda</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Pisonia_grandis">Watha Bhanga</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Avocado"> Avocado</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Lagerstroemia_speciosa">Murutha</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Coconut">Coconut</a></li>
+<li><a href="https://en.wikipedia.org/wiki/Syzygium_caryophyllatum">Dan</a></li>
 
-Why is technology marketing important?
-Every company needs marketing. It's the fuel behind attracting more traffic to your company's website, staying forefront of your customers' minds, 
-and generating new subscribers, customers and leads. <br> <br>
 
-Without Internet marketing, your potential customers may not even see your company in search results when they're looking for the exact products and services you offer. <br> <br>
 
-With effective technology marketing, a company like yours can reach more potential leads, educate those leads about the benefits your services 
-and create long-lasting customers. These customers can provide long-term revenue and profits for your business. <br><br>
 
-Of course, for any company, the challenge is determining which technology product marketing strategies produce the best results. <br><br>
-
-Online platforms have revolutionized the marketing industry and have proven to be quite the game changer for advertising products. However, 
-marketing tools are constantly evolving, so it is important for companies to always update their strategies by keeping up with the latest gadgets and software tools. <br><br>
-
-To take the brand to the next level, a business should apply diverse marketing services that focus on different aspects. 
-Traditional methods focused on television and radio commercials as well as newspaper ads or simply putting marketing material in a mailbox and hoping for the best do not work anymore. <br><br>
-
-Though advertising via television can still have some impact, there are numerous digital marketing strategies that can have a more direct and closer influence on the public. <br><br>
+ 
+ 
+ 
+ </ul>
 </p></section>
 
 </body>
