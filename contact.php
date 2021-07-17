4782e12dbafe3c3567458deb333d9ca98f48a637
uditha-s <albauditha@gmail.com>
2021-07-17 07:40:54 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@
 <?php require_once('inc/header.php'); ?>
   <div class="text-box">
         <h1>Let's Chat</h1>
-        <p>ShareTheMeal is a crowdfunding smartphone application to fight global hunger through the United Nations World Food Programme (WFP).</p>
+        <p>If you have a any idea for increase forest population.just tell us !.</p>
         <a href="" class="visit-btn">Visit us</a>
         </div>
     </section>
