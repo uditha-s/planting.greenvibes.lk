f34e4f997fc661f783b49b9d6e4d8516131a67b7
uditha-s <albauditha@gmail.com>
2021-07-17 10:21:38 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,27 @@
+<style type=text/css> .header {
+    background-image: linear-gradient(rgba(4,9,30,0.7),rgba(4,9,30,0.7)),url(img/planting04.jpg);}
+</style>
+
+<?php $title = 'Contact - '; ?>
+<?php require_once('inc/header.php'); ?>
+  <div class="text-box">
+        <h1>Let's Chat</h1>
+        <p>If you have a any idea for increase forest population.just tell us !</p>
+        <a href="" class="visit-btn">Visit us</a>
+        </div>
+    </section>
+<body>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+<?php require_once('inc/footer.php'); ?>
